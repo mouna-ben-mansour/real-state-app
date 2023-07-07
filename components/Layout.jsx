370559b2ef0849c722d2fafa0864e0813bd7a76e
mouna-ben-mansour <benmansour.mouna2@gmail.com>
2023-07-07 18:09:58 +0100

    display data + style

---
+++ components/Layout.jsx
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
+
+
+const Layout = ({ children }) => (
+    <>
+    <Head>
+        <title>Real Estate</title>
+    </Head>
+    <Box maxWidth="1280px" m="auto">
+        <header>
+           Navbar
+        </header>
+        <main>
+            {children}
+        </main>
+        <footer>
+            Footer
+        </footer>
+    </Box>
+    </>
+);
+export default Layout
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 import { Banner } from './Banner'
 import { fetchApi, baseUrl } from '../utils/fetchApi';
+import  Property  from '../components/Property';
 
 export default function Home({ propertiesForSale, propertiesForRent }) {
   return (
@@ -18,7 +19,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
           imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
         />
         <Flex flexWrap='wrap'>
-         
+         { propertiesForRent.map((property) => <Property property={property} key={property.id}/>)}
         </Flex>
         <Banner
           purpose='BUY A HOME'
@@ -31,7 +32,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
           imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
         />
         <Flex flexWrap='wrap'>
-          
+         { propertiesForSale.map((property) => <Property property={property} key={property.id}/>)}
         </Flex>
       </Box>
   )
